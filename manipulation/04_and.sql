9eb8e36f4960a0909a60a408b0d304036b644ee0
Tommy Chu <tommychu2256@gmail.com>
2020-10-17 23:09:29 +0200

    Added and

---
+++ manipulation/04_and.sql
@@ -0,0 +1,22 @@
+-- and
+
+-- used in where (select, update, delete) + (inner join, left join)
+
+--          TRUE	FALSE	NULL
+--  TRUE	true	false	null
+--  FALSE	false	false	false
+--  NULL	null	false	null
+
+--  select (1 = 0) and (1 / 0);
+  -- sql evaluates only the first expression (false)
+  -- does not print error
+
+select
+  firstname,
+  lastname
+from
+  students
+where
+  age = 18
+  and city = 'New York'
+  and parttime_job = true;
