db0f5739a5bfeb23ed7dd55056162e32cd337849
Tommy Chu <tommychu2256@gmail.com>
2020-10-18 10:27:03 +0200

    Added like clause

---
+++ manipulation/08_like.sql
@@ -0,0 +1,47 @@
+-- like
+-- query data based ona pattern
+-- used with where (select, update, delete)
+
+--  expression [NOT] LIKE pattern ESCAPE escape_character
+
+-- wildcard characters
+  -- % any string of zero or more characters
+  -- _ any single character
+
+-- % wildcard
+select
+  firstname,
+  lastname
+from
+  students
+where
+  lastname like 'A%'; -- lastname starts with A
+
+-- _ wildcard
+select
+  firstname,
+  lastname
+from
+  students
+where
+  firstname like 'T_m'; -- Tim, Tom
+
+-- not like
+select
+  firstname,
+  lastname
+from
+  students
+where
+  firstname not like 'B%';
+
+-- custom escape clause
+select
+  student_id,
+  firstname,
+  lastname
+from
+  students
+where
+  student_id like '%\_2014';
+-- student_id like '%$_2014' escape '$';
