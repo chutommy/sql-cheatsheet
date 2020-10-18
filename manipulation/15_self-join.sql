a877001c7e17b1bbfa69aa2ff337197c6faa9a5f
Tommy Chu <tommychu2256@gmail.com>
2020-10-18 20:05:42 +0200

    Added self join notes

---
+++ manipulation/15_self-join.sql
@@ -0,0 +1,27 @@
+-- right join
+-- join two or more tables
+-- inner join, left join, right join
+-- join single table twice
+
+-- inner join
+SELECT 
+    CONCAT(m.lastName, ', ', m.firstName) AS Manager,
+    CONCAT(e.lastName, ', ', e.firstName) AS 'Direct report'
+FROM
+    employees e
+INNER JOIN employees m ON 
+    m.employeeNumber = e.reportsTo
+ORDER BY 
+    Manager;
+
+-- left join
+SELECT 
+    IFNULL(CONCAT(m.lastname, ', ', m.firstname),
+            'Top Manager') AS 'Manager',
+    CONCAT(e.lastname, ', ', e.firstname) AS 'Direct report'
+FROM
+    employees e
+LEFT JOIN employees m ON 
+    m.employeeNumber = e.reportsto
+ORDER BY 
+    manager DESC;
