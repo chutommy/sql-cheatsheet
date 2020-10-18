154274dbd2efb22350d1d73a99e211fdc4f56e94
Tommy Chu <tommychu2256@gmail.com>
2020-10-18 21:34:38 +0200

    Added cross join

---
+++ manipulation/16_cross-join.sql
@@ -0,0 +1,15 @@
+-- cross join
+-- join two or more tables
+-- include all rows from both tables
+-- each row is the combination of the row in the first table with the row in the second table
+-- n x m rows
+
+--  SELECT * FROM t1
+--  CROSS JOIN t2;
+
+--  SELECT
+--    store_name,
+--    product_name
+--  FROM
+--    stores AS a
+--    CROSS JOIN products AS b;
