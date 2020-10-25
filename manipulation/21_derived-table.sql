96a54cb84a16db207db1f5dba213bc967873f1b3
Tommy Chu <tommychu2256@gmail.com>
2020-10-25 18:11:01 +0100

    Added example of derived table

--- manipulation/21_derived-table.sql
+++ manipulation/21_derived-table.sql
@@ -2,3 +2,25 @@
 -- virtual table returned from a SELECT statement
   -- subquery in FROM clause of SELECT statement\
 -- must have its own alias
+
+SELECT
+  productName,
+  sales
+FROM
+  (
+    SELECT
+      productCode,
+      ROUND(SUM(quantityOrdered * priceEach)) sales
+    FROM
+      orderdetails
+      INNER JOIN orders USING (orderNumber)
+    WHERE
+      YEAR(shippedDate) = 2003
+    GROUP BY
+      productCode
+    ORDER BY
+      sales DESC
+    LIMIT
+      5
+  ) top5products2003
+  INNER JOIN products USING (productCode);
